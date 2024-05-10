b6763e8e27efad2e615f7ea65de08b3dac473917
Aditya Kumar Tiwari <130439857+AdityaKumar-Tiwari@users.noreply.github.com>
2024-05-10 14:12:09 +0000

    datatypes

---
+++ 1_Basics/2_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all code as newe version of JS code
+
+//alert(3 + 3) //we are using node js, not browser
+
+console.log(3+3)
+
+let name = "Aditya"
+let age = 20
+let isloggedin = false
+
+/* number = 2^53
+biginit
+string = ""
+boolean = true/false
+null = standalong value
+undefined = 
+symbol - uniqueness
+
+object =non premitive
+*/
+
+console.log(typeof "Aditya")   //output is string
+console.log(typeof null)      //output is object 
+console.log(typeof undefined)  //output is undefined only
