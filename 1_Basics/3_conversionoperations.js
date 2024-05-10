f7f10bb1321f6ea0b6d5e3ffb7fc791a292e0780
Aditya Kumar Tiwari <130439857+AdityaKumar-Tiwari@users.noreply.github.com>
2024-05-10 14:34:32 +0000

    operations

--- 1_Basics/3_conversionoperations.js
+++ 1_Basics/3_conversionoperations.js
@@ -1,4 +1,4 @@
-let score = 33
+/*let score = 33
 
 console.log(typeof score)
 
@@ -6,3 +6,34 @@ console.log(typeof (score))  //method
 
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber)
+*/
+//operations**************************
+//let value =3
+//let negValue = -value
+//console.log(negValue)
+
+/*
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/2)
+console.log(2%3)
+*/
+
+console.log("1" + 2)   //12
+console.log(1 + "2")  //12
+console.log("1" + 2 + 2)  //122
+console.log(1 + 2 + "2")  //32
+console.log(1 + "2" + 2)  //122
+
+
+console.log(+true)  //1
+console.log(+"")    //0
+
+let num1, num2, num3 
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
