d4df2589c22e750b28da96218f475d17aac5d380
Aditya Kumar Tiwari <130439857+AdityaKumar-Tiwari@users.noreply.github.com>
2024-05-10 17:22:18 +0000

    comparisions

--- 1_Basics/4_comparisions.js
+++ 1_Basics/4_comparisions.js
@@ -2,6 +2,8 @@
 
 //console.log("2" > 4)
 
+
+/*
 console.log(null > 0)
 console.log(null == 0)
 console.log(null  >= 0)
@@ -11,3 +13,6 @@ console.log(undefined > 0)
 console.log(undefined < 0)
 
 console.log("2" === 2)
+*/
+const myFunc = Function('console.log("hello world");')
+myFunc();
