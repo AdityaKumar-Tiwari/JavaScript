5977f9a6255da1009b292e983cc85e45569c9108
Aditya Kumar Tiwari <130439857+AdityaKumar-Tiwari@users.noreply.github.com>
2024-05-17 12:24:12 +0000

    executionphase

---
+++ 3_Basics/jsexecutionphase.js
@@ -0,0 +1,10 @@
+//javascript excecution context
+
+//it is of 3 type
+//1. global execution context = allocate in "this"
+//2. function excecution context
+//3. eval execution  context
+
+//{js code} it runs in two phases
+//1. memory creation phase = here only declaration happens loike declaration of variables
+//2. execution phase  = eecution happens of variables
