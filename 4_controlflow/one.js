6f11d707e50535c65610fd3e691243ff1b8dec63
Aditya Kumar Tiwari <130439857+AdityaKumar-Tiwari@users.noreply.github.com>
2024-05-17 12:43:41 +0000

    one

---
+++ 4_controlflow/one.js
@@ -0,0 +1,31 @@
+//if
+ //const isuserloggedin = true
+//if(condition){
+
+//}
+
+//comparision operators = >,<, <=, >=, ==, !=, ===, !==
+
+//if(2 === "2"){
+  //  console.log("executed")
+//}
+
+//const temp = 50
+//if(temp === 50){
+//    console.log("low")
+//}else{
+//    console.log("temp is high")
+//}
+//console.log("execute")
+
+//shoethand
+//const balance = 1000
+//if(balance > 500) console.log("test")
+
+
+//const useloggedin = true
+//const debitcard = true
+//if(useloggedin && debitcard){
+//    console.log("allow to buy")
+//}
+
